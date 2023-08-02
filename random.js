8f3540778715a318ad15897b282cc53b3c5d82ba
Immanuel <immanuelrajmohan3@gmail.com>
2023-08-02 09:46:04 +0530

    random numbers added

---
+++ random.js
@@ -0,0 +1,44 @@
+// let x=Math.trunc(Math.random()*10)
+// console.log(x);
+
+
+
+
+
+// generate random number between 1 and 6
+// function getRandomNumber(min, max) {
+//     let step1=max-min+1;
+//     let step2=Math.trunc(Math.random()*step1)+min
+//     return step2
+// }
+
+// let random=getRandomNumber(100,200)
+// console.log(random);
+
+
+
+
+
+// get random interger number from 50 to 100
+
+// function getRandomNumberWithin(min, max) {
+//     let num = Math.trunc((Math.random() * max)+min );
+//     return num
+// }
+// console.log(getRandomNumberWithin(50,100));
+
+
+
+// select from array of items
+
+// function getRandomNumber(min, max) {
+//     let step1=max-min+1;
+//     let step2=Math.trunc(Math.random()*step1)+min
+//     return step2
+// }
+
+// let array=['john','alpha','peterson','loi','fred','alice','larry','kevin','james']
+
+// let random=array[getRandomNumber(0,array.length-1)]
+// console.log(random);
+
