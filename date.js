2e10c6e2260e4045628f463cefc628f17b254c30
Immanuel <immanuelrajmohan3@gmail.com>
2024-08-30 09:58:04 +0530

    added date methods and random shuffle function

--- date.js
+++ date.js
@@ -1,11 +1,46 @@
-const currentDate = new Date();
 
 
 
+// const currentDate = new Date();
 
 
 // console.log(currentDate); 
 
+// get the hour
+// console.log(currentDate.getHours()); 
+// console.log(currentDate.getDay()); 
+// console.log(currentDate.getTimezoneOffset()); 
+
+// the date and the year
+// console.log(currentDate.getDate(),currentDate.getFullYear()); 
+
 // console.log(currentDate.getDate(),'/',currentDate.getMonth());
 
 // console.log(currentDate.toLocaleDateString()); 
+
+// console.log(currentDate.getMinutes()); 
+
+// time delta in hours
+
+// var atime = new Date("2024-08-06");
+// var btime = new Date("2024-08-27");
+
+// var delta_time = btime - atime; 
+// var hours = delta_time / (60 * 60 * 1000);
+// var days=hours/24
+// console.log(days);
+
+
+// Setter methods are used to set or update the value of an object's property. They are called "setters" because they set the value of a property.
+
+let date = new Date();
+date.setFullYear(2025);
+date.setDate(15)
+date.setMonth(5)
+date.setMinutes(21)
+
+
+console.log(date.toLocaleString());
+console.log(date.getMinutes());
+
+
--- random.js
+++ random.js
@@ -15,7 +15,8 @@
 // let random=getRandomNumber(100,200)
 // console.log(random);
 
-
+// let random=getRandomNumber(1,7)
+// console.log(random);
 
 
 
@@ -42,3 +43,62 @@
 // let random=array[getRandomNumber(0,array.length-1)]
 // console.log(random);
 
+
+
+// function generateRandom(min,max){
+//     const randomNumber=Math.trunc(Math.random()*(max-min)+1)+min
+//     return randomNumber
+// }
+
+// console.log(generateRandom(11,20))
+
+
+
+
+
+
+function randomChoice(min, max) {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) 
+    return randomNumber;
+}
+
+
+// console.log(randomChoice(0,5))
+
+//  let array=['john','alpha','peterson','loi','fred','alice','larry','kevin','james']
+
+
+//  console.log(array[randomChoice(0,(array.length-1))]);
+ 
+
+// function shuffleArray(array) {
+//         for (let i = 0; i <=array.length - 1; i++) {
+//     //   const j = Math.floor(Math.random() * (i + 1));
+//       const j = Math.floor(Math.random() * (i ));
+//     //  [array[i], array[j]] = [array[j], array[i]];
+//     let temp=array[i]
+//     array[i]=array[j]
+//     array[j]=temp
+//     }
+//      return array;
+   
+//   }
+
+
+//   console.log(shuffleArray([1,2,3,4,5]));
+  
+
+
+// random boolean
+
+
+// const randomBool=Math.random()<0.5
+
+// console.log(randomBool);
+
+
+// const randomBool=Math.floor(Math.random()*10)===4
+// console.log(randomBool);
+
+const randomBool = [4, 5].includes(Math.floor(Math.random() * 10))
+console.log(randomBool);
